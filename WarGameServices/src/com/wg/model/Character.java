d7a933622186df2ce9a295c995e0def21cbf4cd6
garen <enzo@enzo-PC>
2014-08-09 21:59:00 +0200

    user model added

--- WarGameServices/src/com/wg/model/Character.java
+++ WarGameServices/src/com/wg/model/Character.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Character")
+@Table(name="\"Character\"")
 public class Character extends BaseModel{
 
 	@Id
---
+++ WarGameServices/src/com/wg/model/User.java
@@ -0,0 +1,55 @@
+package com.wg.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="\"User\"")
+public class User extends BaseModel {
+
+	@Id
+	@SequenceGenerator(name="User_idUser_seq",
+	sequenceName="User_idUser_seq",
+	allocationSize=1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,
+	generator="User_idUser_seq")
+	@Column(name = "\"idUser\"", updatable=false)
+	private String idUser;
+
+	@Column(name="mail")
+	private String mail;
+
+	@Column(name="password")
+	private String password;
+
+	public String getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(String idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
