a4179a5df451be1ea4d94182da9c8290e30a6eee
garen <garen@libero.it>
2014-09-24 23:53:44 +0200

    processing.js v 2

--- WarGame/WebContent/skins/js/playArea/playArea.js
+++ WarGame/WebContent/skins/js/playArea/playArea.js
@@ -1,10 +1,11 @@
-
-void setup() {
+PImage milit;
+void setup(){
 	size(400,200);
+//	in Processing, you must load the images inside of the setup function
+	milit = loadImage("/WarGame/skins/img/militar.jpg");
 }
-
-void draw() {
-	background(0);
-	fill(255,0,0);
-	ellipse(mouseX,mouseY,50,50);
+void draw(){
+	background(255);
+//	displaying the images simply pass the function the variable and a location
+	image(milit,mouseX,mouseY);
 }
